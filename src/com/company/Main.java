390ad3cc34abc11f2653d673a78939b6e79d30ae
NotteGatto <ast2605@gmail.com>
2021-11-06 21:54:01 +0600

    feature Sun added with a bug that needs to be resolved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,13 @@ public class Main {
         andromeda.planet=2;
         andromeda.moon=4;
 
+        featureSun mikeSun=new featureSun();
+        mikeSun.color="blue";
+        mikeSun.heat="6000034343kj";
+        mikeSun.temp="5celsius";
+        mikeSun.radius="0.009";
+        mikeSun.maritalStatus="single";
+
 
 
 
